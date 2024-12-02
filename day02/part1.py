7c84c64e676ead9c9d93c97e7c0fdb055dfc109d
Samarth Pai <samarthpai9870@gmail.com>
2024-12-02 12:50:41 +0530

    Added day 2 solutions

---
+++ day02/part1.py
@@ -0,0 +1,15 @@
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+    
+safe = 0
+for line in lines:
+    intLine = list(map(int, line.split()))
+    l1, l2 = intLine, intLine[1:]
+    isIncreasing = (l1[1] - l1[0])>0
+    for i, j in zip(l1, l2):
+        if not 1<=abs(i-j)<=3 or isIncreasing and (j - i)<0 or not isIncreasing and (j - i)>0:
+            break
+    else:
+        safe+=1
+        
+print(safe)
