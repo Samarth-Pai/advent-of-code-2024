164fcef353067e370cc3515eb63d8f0404d3c92c
Samarth Pai <samarthpai9870@gmail.com>
2024-12-07 12:10:21 +0530

    Added day 7 solution

---
+++ day07/part1.py
@@ -0,0 +1,27 @@
+from itertools import product
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+    
+def isPossible(target: int, nums: list[int]):
+    if sum(nums) == target:
+        return True
+    n = len(nums)
+    for opers in product("*+", repeat=n-1):
+        total = nums[0]
+        for k, op in zip(nums[1:], opers):
+            if op=="*":
+                total*=k
+            else:
+                total+=k
+            if total==target:
+                return True
+    return False
+
+summ = 0
+for line in lines:
+    target, nums = line.split(":")
+    target = int(target)
+    nums = list(map(int, nums.split()))
+    summ+=isPossible(target, nums)*target
+
+print(summ)
